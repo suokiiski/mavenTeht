0acfad05c74f39c6e1d910a224c275bae4e00263
Korpi <104062080+suokiiski@users.noreply.github.com>
2023-09-18 19:55:26 +0300

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+public class App
+{
+
+    public static double Laske(int fahrenheit) {
+        double celsius;
+
+        celsius = ((fahrenheit - 32) * 5 / 9);
+        System.out.println(celsius);
+        return celsius;
+    }
+
+    public static double Matka(int aloituspaikka, int loppupaikka) {
+        int matka;
+
+        matka = loppupaikka - aloituspaikka;
+        System.out.println("Pisteiden " + aloituspaikka + " ja " + loppupaikka + " välimatka on " + matka);
+        return matka;
+    }
+
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Let's calculate stuff :)");
+
+    }
+}
