c2634cc3216bf39ee8f13c1320f855c8d22fe8ac
Korpi <104062080+suokiiski@users.noreply.github.com>
2023-09-18 19:58:23 +0300

    Add files via upload

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+
+public class AppTest {
+
+    @Test
+    public void TestLaske() {
+        int fahrenheit = 5;
+        double celsius = App.Laske(fahrenheit);
+        double delta = 0.01;
+        assertEquals(-15.0, celsius, delta);
+    }
+
+    @Test
+    public void TestiMatka() {
+        int aloituspaikka = 30;
+        int loppupaikka = 80;
+        double delta = 0.01;
+        double matka = App.Matka(aloituspaikka, loppupaikka);
+        assertEquals(50.0, matka, delta);
+    }
+
+}
